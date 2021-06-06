872a9ab911313e53344d1755c3642d186481d4b4
PrimaryAaron <>
2021-06-06 08:39:48 +0000

    divide

--- CI-Calculator-circleci-project-setup/calculator.py
+++ CI-Calculator-circleci-project-setup/calculator.py
@@ -12,3 +12,6 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
+def divide(a, b):
+    return a / b
--- CI-Calculator-circleci-project-setup/test_calculator.py
+++ CI-Calculator-circleci-project-setup/test_calculator.py
@@ -13,3 +13,6 @@ class TestCalculatorApp:
     
     def test_multiply(self):
       assert 4 == calculator.multiply(2, 2)
+    
+    def test_divide(self):
+      assert 2 == calculator.divide(4, 2)
